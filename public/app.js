7fe1344d3064df263a39063e33b086037bb06634
Beth Fraser <bethanyfraser@live.co.uk>
2016-02-02 17:39:08 +0000

    Region selector added

--- public/app.js
+++ public/app.js
@@ -1,72 +1,76 @@
+Array.prototype.contains = function ( needle ) {
+   for (i in this) {
+       if (this[i] == needle) return true;
+   }
+   return false;
+}
+
 window.onload = function(){
-  console.log('App started');
 
   var url = "https://restcountries.eu/rest/v1";
   var request = new XMLHttpRequest();
   request.open("GET", url);
 
-
-  var select = document.getElementById('dropdown');
+  var select = document.getElementById('countryDropdown');
+  var regionSelect = document.getElementById('regionDropdown');
   var countryInfo = document.getElementById('countryInfo');
 
-  var JSONcountry = localStorage.getItem('saved country') || {};
-
+  var JSONcountry = localStorage.getItem('saved country');
   var countryToLoad = JSON.parse(JSONcountry);
 
-  var li1 = document.createElement('li');
-  li1.innerText = "Country: " + countryToLoad.countryname;
-  countryInfo.appendChild(li1);
+  var makeList = function(name, population, capital){
+    makeListItem("Country: ", name);
+    makeListItem("Capital: ", capital);
+    makeListItem("Population: ", population);
+  }
 
-  var li2 = document.createElement('li');
-  li2.innerText = "Population: " + countryToLoad.countrypopulation;
-  countryInfo.appendChild(li2);
+  var makeListItem = function(description, data){
+    var li = document.createElement('li');
+    li.innerText = description + data;
+    countryInfo.appendChild(li);
+  }
 
-  var li3 = document.createElement('li');
-  li3.innerText = "Capital: " + countryToLoad.countrycapital;
-  countryInfo.appendChild(li3);
-  console.log(countryToLoad);
+  if (countryToLoad) {
+    makeList(countryToLoad.countryname, Number(countryToLoad.countrypopulation).toLocaleString(), countryToLoad.countrycapital);
+  }
+  
 
-
-
+  request.send(null);
 
   request.onload = function(){
-    if(request.status === 200){
-      console.log('got the data');
-    }
+    // if(request.status === 200){
+    //   console.log('got the data');
+    // }
+
+    var regions = [];
 
     var countries = JSON.parse(request.responseText);
-    // console.log("First country:", countries[0].name);
-
-    // console.log("Last country:", countries[countries.length - 1].name);
-    // console.log("Countries over 100 million people:")
-    // for(country of countries){
-    //   if(country.population > 100000000){
-    //     console.log(country.name, country.population);
-    //   }
-    // }
-    // console.log("Northern European countries:")
-    // for(country of countries){
-    //   if(country.subregion === "Northern Europe"){
-    //     console.log(country.name);
-    //   }
-    // }
-
     for(country of countries){
       var listItem = document.createElement("option");
       listItem.innerText = country.name;
       select.appendChild(listItem);
-    }   
-  
 
-  var loadCountryData = function(){
-    var name = select.selectedOptions[0].innerText;
-    countryInfo.innerText = "";
-      
+      if(!regions.contains(country.region)){
+        regions.push(country.region);
+      }
+    }   
+
+    for(region of regions){
+      var listItem = document.createElement("option");
+      listItem.innerText = region || "Other";
+      regionSelect.appendChild(listItem);
+    }
+
+
+
+    var loadCountryData = function(){
+      var name = select.selectedOptions[0].innerText;
+      countryInfo.innerText = "";
+
       for(country of countries){
         if(country.name === name){
           var population = country.population;
           var capital = country.capital;
-
           var countryToSave = {
             countryname: name,
             countrypopulation: population,
@@ -74,39 +78,35 @@ window.onload = function(){
           }
 
           var countryString = JSON.stringify(countryToSave);
-
           localStorage.setItem("saved country", countryString);
 
-          console.log(countryToSave);
-
-          var li1 = document.createElement('li');
-          li1.innerText = "Country: " + name;
-          countryInfo.appendChild(li1);
-
-          var li2 = document.createElement('li');
-          li2.innerText = "Population: " + population;
-          countryInfo.appendChild(li2);
-
-          var li3 = document.createElement('li');
-          li3.innerText = "Capital: " + capital;
-          countryInfo.appendChild(li3);
-
+          makeList(name, Number(population).toLocaleString(), capital);
         }
       }
+    }
 
+    var filterCountries = function(){
+      select.innerHTML = "";
+      var region = regionSelect.selectedOptions[0].innerText;
+      if (region === "Other"){
+        region = "";
+      }
+      for(country of countries){
+        if(country.region === region){
+          var listItem = document.createElement("option");
+          listItem.innerText = country.name;
+          select.appendChild(listItem);
+        }
+      }
+    }
+
+    select.onchange = loadCountryData;
+    regionSelect.onchange = filterCountries;
   }
-
-  select.onchange = loadCountryData;
-
-  }
-
-
-request.send(null);
-
-
- 
-
-
-
 
 };
+
+// show little people for population
+// bordering countries
+//flags? wikipedia
+// 
